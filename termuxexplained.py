82d37a7ef20b2f10e61bb342b49d4a2e65e06e6b
segun-balleriv <70310641+segun-balleriv@users.noreply.github.com>
2020-11-16 01:46:18 +0100

    Add files via upload

---
+++ termuxexplained.py
@@ -0,0 +1,70 @@
+from time import sleep
+import sys
+from os import system
+print("\033[1;32m TOOL BY SEGUN ADEOLA A.K.A SHEGZY_OFFICIAL404")
+sleep (1)
+print ("""\033[1;32m       ╔═══════════╗ 
+   ╔═╝███████████╚═╗
+╔╝███████████████╚╗ 
+║█████████████████║ 
+║█████████████████║ 
+║█████████████████║
+║█╔█████████████╗█║ 
+╚╦╝███▒▒███▒▒███╚╦╝ 
+╔╝██▒▒▒▒███▒▒▒▒██╚╗ 
+║██▒▒▒▒▒███▒▒▒▒▒██║ 
+║██▒▒▒▒█████▒▒▒▒██║ 
+╚╗███████████████╔╝
+╔═╬══╦╝██▒█▒██╚╦══╝.▒.. 
+║█║══║█████████║　...▒.  
+║█║══║█║██║██║█║　.▒..
+║█║══╚═╩══╩╦═╩═╩═╦╗▒.  
+╔╝█╚══╦═╦══╦╩═╦═╦═╩╝
+╔╝█████║█║██║██║█║
+║██████║█████████║\n""")
+sleep(2)
+print("\033[1;32m Hi guys I'm Segun adeola by name. \n I am an admin of the public group called anonymous squad hackers \n I made this tool with love for all the noobs out there especially those without prior knowledge about how termux is being used \n I love you all thanks 💓\n")
+system("clear")
+sleep(4)
+print ("-------------------------Content Table----------------------")
+sleep(1)
+print("\033[1;32m\n[1]: Introduction to termux\n[2]: Who invented the linux terminal emulator\n[3]: Basic termux commands\n[4]: Application of termux commands\n")
+sleep(3)
+print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n")
+sleep(1)
+x=input("\033[1;32m [+]\tchoose an option to start: >>> " )
+if x =="1":
+	print("\033[1;32mGood!!\nLoading tutorials....")
+# read all about the articles
+	print("\033[1;32mTermux is a Linux Terminal Emulator application for Android. A terminal emulator is a program that allows the user to access the command line interface (CLI) in a graphical environment.\nNow we know that termux is a Linux Terminal Emulator. But why it is so famous and sometimes called a hacking machine. Well, that’s somehow true and sometimes a piece of false information also. Termux is not a standalone hacking application. As we discussed, it’s a program that takes instructions and runs scripts. In some Linux distributions like Kali, Parrot OS, we find a lot of tools for penetration testing. Most of them are CLI based and run through the terminal. We can also install various other tools and run different scripts made for Pentesting.\nSo on Android, obviously you get a complete GUI environment. Where these tools won’t work without any command line utility. That is where termux is used to install such tools on your Android Platform. Our Android is also based on a modified Linux kernel. That’s why we can use Bash scripting in it. That’s why termux can easily connect to the file system to perform the majority of the set of features. Which allows you to use the Linux command line to install and run various scripts.")
+else:
+	print("\033[1;32mtermuxexplain: wrong input")
+print("----------------------------------------------------------------")
+sleep(1)
+print("\033[1;32m\n[1]: Introduction to termux\n[2]: Who invented the linux terminal emulator\n[3]: Basic termux commands\n[4]: Application of termux commands\n")
+y=input("\033[1;32m [+]\tchoose an option again from menu: >>> ")
+if y =="2":
+	print("\033[1;32mnice job there")
+	print("\033[1;32mwell this question looks funny lol but the creator of the termux app is popularly known as Sir.Frederick Fornwall alongside some of his colleagues")
+else:
+	print("\033[1;32mtermuxexplain: wrong input")
+sleep(1)
+print("----------------------------------------------------------------")
+sleep(1)
+print("\033[1;32m\n[1]: Introduction to termux\n[2]: Who invented the linux terminal emulator\n[3]: Basic termux commands\n[4]: Application of termux commands\n")
+z=input("\033[1;32m [+]\tchoose an option again from menu: >>> ")
+if z =="3":
+	print("\033[1;32mdoing great$$")
+	print("\033[1;32mTermux – Clear Screen: clear\nTermux – Current Working Directory: pwd\nTermux – Changing Directory: cd /home\nTermux – Move Back Directory: cd .. & cd ../../\nTermux – Create New File: touch <file-name>\nTermux – Create New Directory: mkdir <new-directoy-name>\nTermux – Delete Directory: rmdir <directory-name> or use rm -r <directory-name>\nTermux – Delete File: rm <file-name>\nTermux – Rename File and Directory & Move a File: mv <old-filename> <new-overwirte-filename> mv -r <currnet-file-location> <your-new-destination-directory>\nTermux – Copy File and Directory: cp <filename> <new-filename> cp -r <directory-name> <new-directory-name>\nTermux – File Editor nano and VI: vi <filename> then nano <filename>\nTermux – Read File Content cat command: cat <filename>\nTermux – All running background Process: top\nTermux – Change Permission and Group: chmod (permission-mode) <filename>\t chown (newuser:newgroup) <filename>\nTermux – Clone Source code from Github: git clone <cloning url>\nTermux – Download File wget and curl: wget <download-file-url> ,curl <download-file-url> ,-o <output-filename>\nTermux – history All Previous run Command: history")
+else:
+	print("\033[1;32mtermuxexplain: wrong input")
+print("----------------------------------------------------------------")
+sleep(1)
+print("\033[1;32m\n[1]: Introduction to termux\n[2]: Who invented the linux terminal emulator\n[3]: Basic termux commands\n[4]: Application of termux commands\n")
+q=input("\033[1;32m [+]\tchoose an option again from menu: >>> ")
+if q =="4":
+	print("\033[1;32mAwesome keep learning")
+	print("\033[1;32mTermux commands can be applied in various ways and aspects of life such as:\n[1]: In daily projects \n[2]: For learning eg programming languages like Python , JavaScript or even the bash shell\n[3]: cmon learn the rest yourself")
+else:
+	quit()
+
